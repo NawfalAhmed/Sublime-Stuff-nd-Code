2643d17842af035038e035aad932b046814f88d9
Nawfal Ahmed <dev_nawfal_ahmed@outlook.com>
2020-10-18 11:14:46 +0500

    Update sublime environment

---
+++ SublimeUserPlugins/GitCommands.py
@@ -0,0 +1,60 @@
+import sublime
+import sublime_plugin
+from os.path import split as splitpath
+
+
+class GitStageSelfCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit):
+		_, name = splitpath(self.view.file_name())
+		stage = 'wsl git add "' + name + '"'
+		self.view.window().run_command(
+			"exec", {
+				"shell_cmd": stage,
+				"show_panel": False
+				}
+			)
+
+
+class GitStageSelfDirCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit):
+		stage = 'wsl git add .'
+		self.view.window().run_command(
+			"exec", {
+				"shell_cmd": stage,
+				"show_panel": False
+				}
+			)
+
+
+class NameInputHandler(sublime_plugin.TextInputHandler):
+
+	def initial_text(self):
+		return "--unspecified user_commit"
+
+
+class GitCommitCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit, name):
+		commit = 'wsl git commit -m "' + name + '"'
+		self.view.window().run_command(
+			"exec", {
+				"shell_cmd": commit,
+				"show_panel": False
+				}
+			)
+
+	def input(self, args):
+		if "name" not in args:
+			return NameInputHandler()
+		else:
+			return None
+
+	def input_description(self):
+		return "change is: "
+
+
+# --unspecified user_commit
+# --updates to sublime environment
+# --update
--- SublimeUserPlugins/OpenKeyBindings.py
+++ SublimeUserPlugins/OpenKeyBindings.py
@@ -5,6 +5,5 @@ import sublime_plugin
 class OpenKeyBindingsCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit):
-		file_path = sublime.packages_path(
-		) + "\\User\\Default (Windows).sublime-keymap"
+		file_path = "Redacted"
 		self.view.window().open_file(file_path)
