b9af34370f15f51eba6e31507620dd3bca760b5b
Penguin98kStudio <Penguin98kStudio@users.noreply.github.com>
2020-10-24 02:57:56 +0500

    Added updates to Sublime Environment --including build systems

--- SublimeUserPlugins/AutoSideBarReveal.py
+++ SublimeUserPlugins/AutoSideBarReveal.py
@@ -1,7 +1,9 @@
 import sublime
 import sublime_plugin
 
+
 class AutoRevealCommand(sublime_plugin.EventListener):
-	def on_activated(self,view):
+
+	def on_activated(self, view):
 		if view.window().is_sidebar_visible():
 			view.window().run_command("reveal_in_side_bar")
--- SublimeUserPlugins/SmoothNew.py
+++ SublimeUserPlugins/SmoothNew.py
@@ -29,7 +29,7 @@ class SmoothNewCommand(sublime_plugin.TextCommand):
 		window = view.window()
 		window.run_command(
 			"exec", {
-				"shell_cmd": "subl " + name + " && subl",
+				"shell_cmd": 'subl "' + name + '" && subl',
 				"show_panel": False
 				}
 			)
