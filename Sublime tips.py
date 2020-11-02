1909969c5ee5ca940f0f20f6226fdbc0dff300a0
Nawfal Ahmed <dev_nawfal_ahmed@outlook.com>
2020-11-02 18:02:52 +0500

    reformatted the tips and notes files

---
+++ Sublime tips.py
@@ -0,0 +1,38 @@
+class Console:
+	sublime.log_input(True)
+	sublime.log_commands(True)
+	sublime.find_resources('*.sublime-commands')
+	sublime.yes_no_cancel_dialog("Do You Want To Create Input File?")
+
+class KeyBindings:
+	#Try Changning word seperators in settings
+	#Syntax Specific KeyBinding
+	"context": [{ "key": "selector", "operator": "equal", "operand": "text.html.markdown" }],
+
+class Snippets:
+	#Description in Snippets
+	"<description>For Enumerate Loop</description>"
+	#Location Specifing
+	"${1:this}"
+
+class Builds:
+{
+	"shell_cmd": "DOSBoxPortable.exe.lnk",
+	"working_dir": "${file_path}",
+	"selector": "source.assembly",
+	"file_patterns" : "*.asm",
+}
+class Macros:
+[
+	{
+		"args": null,
+		"command": "set_mark"
+	},
+	{
+		"args":
+		{
+			"to": "line"
+		},
+		"command": "expand_selection"
+	},
+]
