41fe98694917055b5d04b0b842ab43f9abc59f13
Penguin98kStudio <Penguin98kStudio@users.noreply.github.com>
2020-11-06 12:45:34 +0500

    Updated Sublime Color Schemes

--- SublimeUserPlugins/AutoSideBarReveal.py
+++ SublimeUserPlugins/AutoSideBarReveal.py
@@ -6,4 +6,5 @@ class AutoRevealCommand(sublime_plugin.EventListener):
 
 	def on_activated(self, view):
 		if view.window().is_sidebar_visible():
-			view.window().run_command("reveal_in_side_bar")
+			if view.settings().get("auto_sidebar_reveal", True):
+				view.window().run_command("reveal_in_side_bar")
