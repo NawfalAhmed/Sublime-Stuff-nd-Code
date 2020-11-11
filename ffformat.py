168713b6f29188daa5055b562cb58c1f56480e57
Nawfal Ahmed <dev_nawfal_ahmed@outlook.com>
2020-11-11 20:43:23 +0500

    Updates to Sublime Environment

---
+++ ffformat.py
@@ -0,0 +1,11 @@
+[
+	{ "caption": "Run Python in Terminal", "command": "run_on_terminal"},
+	{
+		"caption": "Execute Command (At Current Directory)",
+		"command": "execute_cmd"
+	}, { "caption": "User: Toggle Preferences", "command": "toggle_preferences"},
+	{ "caption": "File Commands List", "command": "file_commands"},
+	{ "caption": "Git - Commit Changes", "command": "git_commit"},
+	{ "caption": "User: Test", "command": "user_test"},
+	{ "caption": "New File", "command": "smooth_new"}
+]
