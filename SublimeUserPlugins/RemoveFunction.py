acebd5a2b6701561ed4115a4d383745eb9c6c5d0
Penguin98kStudio <Penguin98kStudio@users.noreply.github.com>
2021-04-15 14:28:19 +0500

    Improved RemoveFunction
    added an option to copy the removed function to the clipboard

--- SublimeUserPlugins/RemoveFunction.py
+++ SublimeUserPlugins/RemoveFunction.py
@@ -4,10 +4,12 @@ import sublime_plugin
 
 class RemoveFunctionCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
+	def run(self, edit, copy=False):
 		self.view.run_command("expand_selection", {"to": "brackets"})
 		selections = [self.view.substr(region) for region in self.view.sel()]
 		self.view.run_command("expand_region")
 		self.view.run_command("expand_region")
 		for region, text in zip(self.view.sel(), selections):
+			if copy:
+				sublime.set_clipboard(self.view.substr(region).replace(text, ""))
 			self.view.replace(edit, region, text)
