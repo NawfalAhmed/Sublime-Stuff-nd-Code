9a4e1d1c4567b830af059f8e7627c409b1d632e0
Penguin98kStudio <Penguin98kStudio@users.noreply.github.com>
2021-04-29 22:28:37 +0500

    Updated NewUserPlugin

--- SublimeUserPlugins/NewUserPlugin.py
+++ SublimeUserPlugins/NewUserPlugin.py
@@ -1,35 +1,46 @@
 import sublime
 import sublime_plugin
 
+_plugin_template = """
+import sublime
+import sublime_plugin
+
+
+class ${1:%s}Command(sublime_plugin.${2:Text}Command):
+	def run(self${3:, edit}):
+		${4:self.view.insert(edit, 0, "Hello, World!")}
+
+"""
+
 
 class NameInputHandler(sublime_plugin.TextInputHandler):
 
-	def initial_text(self):
-		ext = '.py'
-		return ext
+	def placeholder(self):
+		return "Plugin Name"
 
 
 class NewUserPluginCommand(sublime_plugin.WindowCommand):
 
 	def run(self, name):
-		new_file = (
+		sublime.set_timeout_async(lambda: self.run_async(name))
+
+	def run_async(self, name):
+		filename = (
 			"C:/Users/Ibraheem/Desktop/SublimeText/SublimeUserPlugins/" + name
+			+ ".py"
 		)
-		view = self.window.open_file(new_file)
-		view.set_scratch(True)
-		view.run_command(
-			"set_file_type", {"syntax": "Packages/Python/Python.sublime-syntax"}
-		)
-		sublime.set_timeout(
-			lambda: view.run_command(
-				"insert_snippet",
-				{"name": "Packages/User/Snippets/NewPlugin.sublime-snippet"},
-			), 150
-		)
-		sublime.set_timeout_async(lambda: view.set_scratch(False), 60000)
+		view = self.window.open_file(filename)
+		while view.is_loading():
+			pass
+		if not view.size():
+			view.set_scratch(True)
+			view.set_syntax_file("Packages/Python/Python.sublime-syntax")
+
+			view.run_command(
+				"insert_snippet", {"contents": _plugin_template[1:] % name}
+			)
+			sublime.set_timeout_async(lambda: view.set_scratch(False), 60000)
 
 	def input(self, args):
 		if "name" not in args:
 			return NameInputHandler()
-		else:
-			return None
