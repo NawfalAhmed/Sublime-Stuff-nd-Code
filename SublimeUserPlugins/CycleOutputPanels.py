896fa89937265a5189ee08331647d58094484363
Nawfal Ahmed <dev_nawfal_ahmed@outlook.com>
2021-04-29 22:29:39 +0500

    Updated CycleOutputPanels

--- SublimeUserPlugins/CycleOutputPanels.py
+++ SublimeUserPlugins/CycleOutputPanels.py
@@ -6,7 +6,9 @@ class CycleOutputPanelsCommand(sublime_plugin.WindowCommand):
 
 	def run(self):
 		exclude_panels = [
-			"output.diagnostics", "output.find_results", "output.Linux Terminal",
+			# "output.diagnostics",
+			"output.find_results",
+			"output.Linux Terminal",
 			"output.Windows Terminal"
 		]
 
@@ -15,11 +17,11 @@ class CycleOutputPanelsCommand(sublime_plugin.WindowCommand):
 			if "output." == name[:7] and name not in exclude_panels
 		]
 		if output_panels:
-			msg = None
 			if len(output_panels) == 1:
-				msg = "Alert: Only a single Ouput Panel ("
+				msg = "Alert: Only a single Output Panel (%s) exists"
 				index = -1
 			else:
+				msg = "Opened Panel: %s"
 				try:
 					index = output_panels.index(self.window.active_panel())
 				except ValueError:
@@ -28,10 +30,8 @@ class CycleOutputPanelsCommand(sublime_plugin.WindowCommand):
 			index = 0 if (index + 1) == 0 else (index+1) % len(output_panels)
 
 			name = output_panels[index][7:]
-			name = "Build Results" if "exec" == name else name
-			msg = msg + name + ") exists" if msg else "Opened Panel: " + name
-
+			name = name.replace("exec", "Build Results")
 			self.window.run_command("show_panel", {"panel": output_panels[index]})
-			sublime.status_message(msg)
+			sublime.status_message(msg % name)
 		else:
 			sublime.status_message("Error: No Ouput Panel Exists")
