34ae0eb8e8968172f59a8f57fbb2a3bb7a7b8f92
Penguin98kStudio <Penguin98kStudio@users.noreply.github.com>
2021-05-01 23:32:55 +0500

    Added ConvertImportStatement

---
+++ SublimeUserPlugins/ConvertImportStatement.py
@@ -0,0 +1,23 @@
+import sublime
+import sublime_plugin
+
+
+class ConvertImportStatementCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+
+		selection = self.view.sel()
+		change = False
+		for region in selection:
+			for line in reversed(self.view.lines(region)):
+				pattern = "meta.statement.import.python"
+				if self.view.score_selector(line.begin(), pattern):
+					contents = self.view.substr(line)
+					contents = contents.replace("import", "from") + " import "
+					self.view.replace(edit, line, contents)
+					change = True
+		if change:
+			selection = self.view.sel()
+			regions = list(selection)
+			selection.clear()
+			regions = [sublime.Region(line.end()) for region in regions for line in self.view.lines(region)]
+			selection.add_all(regions)
