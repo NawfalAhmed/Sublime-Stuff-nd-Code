3ef8aebf7e5b394d7fbc2b9e1f014f89d5fb8130
Nawfal Ahmed <dev_nawfal_ahmed@outlook.com>
2021-05-03 22:24:53 +0500

    Updated Keybinds (Super->Ctrl+k)

--- SublimeUserPlugins/ContextsListener.py
+++ SublimeUserPlugins/ContextsListener.py
@@ -1,7 +1,8 @@
 import sublime
 import sublime_plugin
 
-class TerminusViewListener(sublime_plugin.EventListener):
+
+class ContextsListener(sublime_plugin.EventListener):
 
 	def on_query_context(self, view, key, operator, operand, match_all):
 		if key == "terminus_tag.exists":
@@ -9,3 +10,6 @@ class TerminusViewListener(sublime_plugin.EventListener):
 				view.settings().get("terminus_view.tag") == operand
 				for view in view.window().views()
 			)
+		elif key == "sidebar_visible":
+			visible = view.window().is_sidebar_visible()
+			return (not visible) if bool(operand) == operator else visible
