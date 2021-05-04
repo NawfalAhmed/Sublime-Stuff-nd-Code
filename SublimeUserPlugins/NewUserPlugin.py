52325f55aae4a58cb6dd704563f5623f7d349c28
Penguin98kStudio <Penguin98kStudio@users.noreply.github.com>
2021-05-04 16:22:56 +0500

    Updated NewUserPlugin

--- SublimeUserPlugins/NewUserPlugin.py
+++ SublimeUserPlugins/NewUserPlugin.py
@@ -1,22 +1,20 @@
 import sublime
 import sublime_plugin
-
-_plugin_template = """
-import sublime
-import sublime_plugin
-
-
-class ${1:%s}Command(sublime_plugin.${2:Text}Command):
-	def run(self${3:, edit}):
-		${4:self.view.insert(edit, 0, "Hello, World!")}
-
-"""
+import re
+import textwrap
 
 
 class NameInputHandler(sublime_plugin.TextInputHandler):
 
 	def placeholder(self):
 		return "Plugin Name"
+
+	def validate(self, text):
+		return bool(re.match(r"^[A-Z][a-zA-Z0-9]+$", text))
+
+	def preview(self, text):
+		if text and not re.match(r"^[A-Z][a-zA-Z0-9]*$", text):
+			return "Invalid Name, Please Enter Name in PascalCase"
 
 
 class NewUserPluginCommand(sublime_plugin.WindowCommand):
@@ -29,6 +27,17 @@ class NewUserPluginCommand(sublime_plugin.WindowCommand):
 			"C:/Users/Ibraheem/Desktop/SublimeText/SublimeUserPlugins/" + name
 			+ ".py"
 		)
+		plugin_template = """
+			import sublime
+			import sublime_plugin
+
+
+			class ${1:%s}Command(sublime_plugin.${2:Text}Command):
+				def run(self${3:, edit}):
+					${4:self.view.insert(edit, 0, "Hello, World!")}
+
+		"""
+		plugin_template = textwrap.dedent(plugin_template).lstrip()
 		view = self.window.open_file(filename)
 		while view.is_loading():
 			pass
@@ -36,9 +45,7 @@ class NewUserPluginCommand(sublime_plugin.WindowCommand):
 			view.set_scratch(True)
 			view.set_syntax_file("Packages/Python/Python.sublime-syntax")
 
-			view.run_command(
-				"insert_snippet", {"contents": _plugin_template[1:] % name}
-			)
+			view.run_command("insert_snippet", {"contents": plugin_template % name})
 			sublime.set_timeout_async(lambda: view.set_scratch(False), 60000)
 
 	def input(self, args):
