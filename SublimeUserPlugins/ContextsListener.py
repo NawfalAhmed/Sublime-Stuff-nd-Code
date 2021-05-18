424861afb95e35f7738c148ab3a65deb8964d501
Nawfal Ahmed <dev_nawfal_ahmed@outlook.com>
2021-05-18 10:23:33 +0500

    Added TerminusPanelVisible ContextListener

--- SublimeUserPlugins/ContextsListener.py
+++ SublimeUserPlugins/ContextsListener.py
@@ -10,6 +10,11 @@ class ContextsListener(sublime_plugin.EventListener):
 				view.settings().get("terminus_view.tag") == operand
 				for view in view.window().views()
 			)
+
+		elif key == "terminus_panel_visible":
+			panel = view.window().active_panel()
+			visible = panel and "Terminal" in panel
+			return (not visible) if bool(operand) == operator else visible
 		elif key == "sidebar_visible":
 			visible = view.window().is_sidebar_visible()
 			return (not visible) if bool(operand) == operator else visible
