1419eba67c17a64c1451313b60efddb0a6a430db
Penguin98kStudio <Penguin98kStudio@users.noreply.github.com>
2021-05-18 10:17:00 +0500

    Added ShiftToMark Macro
    Separated SetUserMark and TransposePro into their own files

--- SublimeUserPlugins/TransposePro.py
+++ SublimeUserPlugins/TransposePro.py
@@ -2,25 +2,6 @@ import sublime
 import sublime_plugin
 
 
-class SetUserMarkCommand(sublime_plugin.TextCommand):
-
-	def run(self, edit, clear=False):
-		if clear:
-			self.view.erase_regions("user_mark")
-			return
-		marks = list(self.view.sel())
-		marks.extend(self.view.get_regions("user_mark"))
-		self.view.add_regions(
-			"user_mark",
-			marks,
-			scope="region.bluish",
-			flags=(
-				sublime.DRAW_SOLID_UNDERLINE | sublime.DRAW_NO_OUTLINE
-				| sublime.DRAW_NO_FILL | sublime.DRAW_EMPTY
-			)
-		)
-
-
 class TransposeProCommand(sublime_plugin.TextCommand):
 
 	stored_marks = None
