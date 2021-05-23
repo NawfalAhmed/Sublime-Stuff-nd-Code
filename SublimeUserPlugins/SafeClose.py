00f5e01bf6fceef434b90afc3bb75be49c09cd59
Penguin98kStudio <Penguin98kStudio@users.noreply.github.com>
2021-05-23 14:39:17 +0500

    Enhancements to Plugins
    CopyLines now ensures cursor visibility
    SafeClose now only prevents closing if its a project window
    StatusBarTime now logs SublimeUpTimes

--- SublimeUserPlugins/SafeClose.py
+++ SublimeUserPlugins/SafeClose.py
@@ -6,7 +6,10 @@ class SafeCloseCommand(sublime_plugin.WindowCommand):
 
 	def run(self):
 		if self.window.num_groups() <= 1 and not self.window.sheets():
-			self.window.status_message("Prevented Project Closing")
+			if self.window.project_file_name():
+				self.window.status_message("Prevented Project Closing")
+			else:
+				self.window.run_command("close")
 		else:
 			if self.window.sheets_in_group(self.window.active_group()):
 				self.window.run_command("close")
--- SublimeUserPlugins/StatusBarTime.py
+++ SublimeUserPlugins/StatusBarTime.py
@@ -1,9 +1,13 @@
 import sublime
+import sublime_plugin
 from time import sleep
 from threading import Thread
 from datetime import datetime
+from os.path import expandvars
 
+log_path = expandvars(r"%Sublime%") + "/SublimeUpTime.log"
 alive = False
+total_up_time = None
 
 
 def plugin_loaded() -> None:
@@ -25,9 +29,24 @@ def plugin_loaded() -> None:
 def plugin_unloaded() -> None:
 	global alive
 	alive = False
+	with open(log_path, "a") as logger:
+		time = datetime.now()
+		log = total_up_time + time.strftime(" on %A _ %B, %Y (logged@%#I:%#M|)")
+		check = {"1": "st", "2": "nd", "3": "rd"}
+		day = time.strftime("%#d")
+		day += check.get(day[-1], "th")
+		log = log.replace("_", day).replace("|", time.strftime("%p").lower())
+		logger.write(log + "\n")
+
+
+class OnExitListener(sublime_plugin.EventListener):
+
+	def on_exit(self):
+		plugin_unloaded()
 
 
 def time_printer(formatt, up_time, update_interval, start_time):
+	global total_up_time
 	while alive:
 		time = datetime.now()
 		view = sublime.active_window().active_view()
@@ -35,7 +54,6 @@ def time_printer(formatt, up_time, update_interval, start_time):
 		day, hr, minute = map(lambda x, y: int(x) - int(y), current, start_time)
 		view.set_status("__statustime", time.strftime(formatt))
 		if up_time:
-			view.set_status(
-				"__statusuptime", str(day*24*60 + hr*60 + minute) + " minutes"
-			)
+			total_up_time = str(day*24*60 + hr*60 + minute) + " minutes"
+			view.set_status("__statusuptime", total_up_time)
 		sleep(update_interval)
