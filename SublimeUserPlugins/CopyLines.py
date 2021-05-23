8416fc1b8a07564a75f806533cf9946611735a6b
Nawfal Ahmed <dev_nawfal_ahmed@outlook.com>
2021-05-23 14:39:17 +0500

    Enhancements to Plugins
    CopyLines now ensures cursor visibility
    SafeClose now only prevents closing if its a project window
    StatusBarTime now logs SublimeUpTimes

--- SublimeUserPlugins/CopyLines.py
+++ SublimeUserPlugins/CopyLines.py
@@ -1,4 +1,4 @@
-# import sublime
+import sublime
 import sublime_plugin
 
 
@@ -14,28 +14,7 @@ class CopyLinesCommand(sublime_plugin.TextCommand):
 			else:
 				self.view.insert(edit, line.end(), "\n" + line_contents)
 
-
-# import sublime, sublime_plugin
-# from sublime import Region
-
-# class DuplicateLinesCommand(sublime_plugin.TextCommand):
-#     def run(self, edit, up = False):
-#         last_caret_region = [(selection.begin(), selection.end()) for selection in self.view.sel()]
-#         for region in self.view.sel():
-#             # if there's a selection, and ends with "\n" we don't want to also duplicate the next line
-#             if not region.empty():
-#                 if "\n" in self.view.substr(region)[-1]:
-#                     region = Region(region.begin(), region.end() - 1)
-
-#             line = self.view.line(region)
-#             line_contents = self.view.substr(line)
-
-#             if up:
-#               self.view.insert(edit, line.end(), "\n" + line_contents)
-#             else:
-#               self.view.insert(edit, line.begin(), line_contents + "\n")
-
-#         if up:
-#             self.view.sel().clear()
-#             for selection in last_caret_region:
-#                 self.view.sel().add(sublime.Region(selection[0], selection[1]))
+		visible = self.view.lines(self.view.visible_region())
+		visible = sublime.Region(visible[0].begin(), visible[-2].end() - 1)
+		if not any(map(visible.contains, self.view.sel())):
+			self.view.show(self.view.sel()[0], show_surrounds=False, animate=False)
