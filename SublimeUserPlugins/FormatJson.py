5a26864d365974368d30f81700c08ef55471ab7b
Penguin98kStudio <denawfal@outlook.com>
2021-06-02 16:00:00 +0500

    Updates to SublimeEnvironment
    Added AnnotationExample in Sublime Tips
    Added Config File for Terminus
    Added ExpandSelectionToBracket Macros (Left or Right)
    Added itertools.filterfalse and .cycle to Ipython_Imports
    Added JumpSelectionEnd Plugin
    Added Keybind for SaveMacro command
    Added Keybinds with altgr modifier
    Enhanced FormatJson (added option: ExpandSelectionToLine)
    Enhanced SetUserMark (added option: convert marks to selections)
    Experimental: Remapped Some Characters (;:\_|.)

--- SublimeUserPlugins/FormatJson.py
+++ SublimeUserPlugins/FormatJson.py
@@ -3,9 +3,8 @@ import sublime_plugin
 import re
 import textwrap
 from threading import Thread
-from subprocess import check_output as syscalloutput, CalledProcessError
+from subprocess import run
 from os.path import expanduser
-# from subprocess import check_call as syscall
 
 
 def run_async(select, view):
@@ -24,7 +23,7 @@ def run_async(select, view):
 			contents = view.substr(sublime.Region(0, view.size()))
 			file.write(fixstr(contents))
 		else:
-			regions = view.sel()
+			regions = map(view.line,view.sel()) if select=="lines" else view.sel()
 			for region in regions:
 				contents = view.substr(region)
 				match = re.match(r"[\s\n]+", contents)
@@ -33,13 +32,54 @@ def run_async(select, view):
 				file.write("\n# end region\n")
 
 	try:
-		contents = syscalloutput("yapf " + name + " --style " + style, shell=True)
-	except CalledProcessError:
-		sublime.status_message(
-			"JsonFormatter ran into an error while trying to format :'("
+		yapf_result = run(
+			f"yapf {name} --style {style}",
+			shell=True,
+			capture_output=True,
+			text=True
 		)
+		contents = yapf_result.stdout
+		yapf_result.check_returncode()
+	except CalledProcessError:
+		annotation = textwrap.dedent('''
+			<body>
+				<style>
+					#annotation-error {
+						background-color: color(var(--background) blend(#fff 95%));
+					}
+					html.dark #annotation-error {
+						background-color: color(var(--background) blend(#fff 95%));
+					}
+					html.light #annotation-error {
+						background-color: color(var(--background) blend(#000 85%));
+					}
+					a {
+						text-decoration: inherit;
+					}
+				</style>
+				<div class="error" id=annotation-error>
+					<span class="content">
+						__content__
+					</span>
+				</div>
+			</body>
+		''').lstrip()  # yapf: disable
+		view.add_regions(
+			"format_error", [sublime.Region(view.line(view.sel()[0]).begin()+1)],
+			scope="invalid",
+			annotations=[
+				annotation.replace('__content__', "Invalid Syntax")
+			],
+			flags=(sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE),
+			on_close=lambda:
+			(view.erase_regions("format_error"), view.hide_popup())
+		)
+		sublime.status_message(
+			"FormatJson's dependency 'yapf' ran into an error while trying to format :'("
+		)
+		print(yapf_result.stderr)
 		return
-	contents = contents.decode().replace("\r", "").replace("#", "//")
+	contents = contents.replace("\r", "").replace("#", "//")
 	contents = contents.replace("True", "true").replace("False", "false")
 	sublime.set_timeout_async(
 		lambda: view.run_command(
@@ -52,20 +92,24 @@ def run_async(select, view):
 class FormatJsonCommand(sublime_plugin.WindowCommand):
 
 	def run(self, select=False):
-		Thread(target=run_async, args=(select, self.window.active_view())).start()
+		Thread(
+			target=run_async,
+			args=(select, self.window.active_view()),
+			name="FormatJson.run_async"
+		).start()
 
 
 class FormatJsonStep2Command(sublime_plugin.TextCommand):
 
 	def run(self, edit, select, contents, indents):
-		print(indents)
 		contents = re.sub(r'("keys": \[.*\],)\s*("command")', r'\1 \2', contents)
 		contents = re.sub(r'([^\s])}', r'\1 }', contents)
 		if select:
 			contents = list(filter(None, contents.split("\n// end region\n")))
-			print(contents)
-			regions = self.view.sel()
-			zipped = zip(reversed(regions), reversed(contents), reversed(indents))
+			regions = reversed(self.view.sel())
+			if select == 'lines':
+				regions = map(self.view.line, regions)
+			zipped = zip(regions, reversed(contents), reversed(indents))
 			for region, region_contents, indent in zipped:
 				if indent:
 					region_contents = textwrap.indent(region_contents, indent)
