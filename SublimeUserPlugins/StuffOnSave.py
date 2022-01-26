d1e5997a181ad276bc5858487b34e086f5c6edd7
Nawfal Ahmed <dev_nawfal_ahmed@outlook.com>
2022-01-26 18:19:37 +0500

    Fixed StuffOnSave and TypeCheck PyBuild

--- SublimeUserPlugins/StuffOnSave.py
+++ SublimeUserPlugins/StuffOnSave.py
@@ -24,14 +24,15 @@ class StuffOnSave(sublime_plugin.ViewEventListener):
 	def formatter_async(self):
 		while True:
 			name = self.format_queue.get()
-			run(f"black -l 85 {name}", shell=True)
-			sleep(3)
+			run(f"black -l 85 '{name}'", shell=True)
+			sleep(2)
 			sublime.status_message(f"Formatted: {name}")
 			self.format_queue.task_done()
 
 	def on_post_save(self):
 		if not self.view.settings().get("python_format_on_save"):
 			return
+
 		name, _, ext = self.view.file_name().rpartition("/")[-1].rpartition(".")
 		if ext == "py":
 			if self.format_queue is None:
