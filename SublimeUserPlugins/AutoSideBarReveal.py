54341be6035e0b8b9c09a69a8ed1f4913a0508cd
Nawfal Ahmed <dev_nawfal_ahmed@outlook.com>
2022-12-04 16:38:36 +0500

    Changes for Arbisoft

--- SublimeUserPlugins/AutoSideBarReveal.py
+++ SublimeUserPlugins/AutoSideBarReveal.py
@@ -4,7 +4,7 @@ import sublime_plugin
 
 class AutoRevealCommand(sublime_plugin.EventListener):
 	flag = 1
-	reset_rate = 15
+	reset_rate = 30
 
 	def reset_sidebar_expansion(self, window):
 		flag = not bool(self.flag % self.reset_rate)
@@ -22,10 +22,10 @@ class AutoRevealCommand(sublime_plugin.EventListener):
 		if view.window().is_sidebar_visible():
 			if view.settings().get("auto_sidebar_reveal", True):
 				window = view.window()
-				if self.reset_sidebar_expansion(window):
-					sublime.status_message("resetted sidebar")
-					sublime.set_timeout(
-						lambda: window.run_command("reveal_in_side_bar"), 200
-					)
-				else:
-					window.run_command("reveal_in_side_bar")
+				# if self.reset_sidebar_expansion(window):
+				# 	sublime.status_message("resetted sidebar")
+				# 	sublime.set_timeout(
+				# 		lambda: window.run_command("reveal_in_side_bar"), 200
+				# 	)
+				# else:
+				window.run_command("reveal_in_side_bar")
